e2761ff54e5cab39a403a3c427709984913ba249
890165 <890165@PC426969.cts.com>
2021-05-25 16:04:45 +0530

    Initial Commit

---
+++ Working_with_DataTypes_(3)/src/com/sample/Math_Test.java
@@ -0,0 +1,30 @@
+package com.sample;
+
+public class Math_Test 
+
+{
+
+	public static void main(String[] args) 
+	
+	{
+		double a=2.4;
+		double b=3;
+		
+		
+		System.out.println("Index\tResult");
+		System.out.println("a+b\t"+(a+b));
+		System.out.println("a-b\t"+(a-b));
+		System.out.println("a*b\t"+(a*b));
+		System.out.println("a/b\t"+(a/b));
+		System.out.println("Max of a and b\t"+Math.max(a, b));
+		System.out.println("Min of a and b\t"+Math.min(a, b));
+		System.out.println("Absolute pf a\t"+Math.abs(a));
+		System.out.println("Floor value of a\t"+Math.floor(a));
+		System.out.println("Logaithmic value of a\t"+Math.log(a));
+		System.out.println("The power term of a and b is\t"+Math.pow(a, b));
+		System.out.println("Sin value of a\t"+Math.sin(a));
+		System.out.println("Square root of a\t"+Math.sqrt(a));
+
+	}
+
+}
---
+++ Working_with_DataTypes_(3)/src/com/sample/String_concat.java
@@ -0,0 +1,19 @@
+package com.sample;
+
+public class String_concat 
+
+{
+
+	public static void main(String[] args) 
+	
+	{
+		double speed=45;
+		double distance_covered=235;
+		
+		System.out.print("The vechicle runs with the speed of "+speed);
+		System.out.print("km/h and covered a distance of "+distance_covered+"km. ");
+		System.out.print("Journey Completed !!!");
+
+	}
+
+}
