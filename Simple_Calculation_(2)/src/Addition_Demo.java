71762fef8ce0c62d82c2805246c13f8018769631
890165 <890165@PC426969.cts.com>
2021-05-25 16:03:49 +0530

    Initial Commit

---
+++ Simple_Calculation_(2)/src/Addition_Demo.java
@@ -0,0 +1,16 @@
+
+public class Addition_Demo
+
+{
+
+	public static void main(String[] args) 
+	
+	{
+		int sum=1+2+3+4+5;
+		
+		float average=sum/5;
+		
+		System.out.println("The average is "+average);
+	}
+
+}
---
+++ Simple_Calculation_(2)/src/Square_demo.java
@@ -0,0 +1,18 @@
+
+public class Square_demo 
+
+{
+
+	public static void main(String[] args) 
+	
+	{
+		double first_value=2.5;
+		double second_value=5.6;
+		
+		double square_result=first_value * second_value;
+		
+		System.out.println("The square_result is "+square_result);
+
+	}
+
+}
